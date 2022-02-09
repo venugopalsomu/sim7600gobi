4b0f5eb296a2352c640bf9f4da2abdaf748a87d6
venugopalsomu <venugopal.somu@gmail.com>
2022-02-09 22:33:37 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+wget https://github.com/venugopalsomu/sim7600gobi/archive/refs/heads/main.zip
+unzip main.zip
+cd sim7600gobi-main/
+cp start.sh ~/sim7600/start.sh
+cp simcom-cm ~/sim7600/simcom-cm
+cp sim7600.service ~/sim7600/
+
+cd ~/sim7600
+chmod +x start.sh
+chmod +x simcom-cm
+sudo cp simcom-cm /usr/bin/
+
+sudo cp sim7600.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable sim7600.service
